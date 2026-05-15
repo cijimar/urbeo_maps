1bec06a253752f0c24220953dcd47d57580c9cd6
Cintia Jiménez <cijimar@gmail.com>
2026-05-15 12:52:36 +0200

    commit inicial

---
+++ src/main/java/com/urbeo/urbeo_backend/UrbeoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urbeo.urbeo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbeoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbeoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbeo/urbeo_backend/UrbeoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbeo.urbeo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbeoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
